bdf1936bdf9df7adeaa8b72df9c360a9b1193254
jimmys20 <maragkosd12@gmail.com>
2022-09-07 00:10:48 +0300

    Add project files.

---
+++ SO73601166/Models/Fruit.cs
@@ -0,0 +1,9 @@
+﻿namespace SO73601166.Models
+{
+    public class Fruit
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ SO73601166/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SO73601166.Models;
+
+namespace SO73601166.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly List<Fruit> _fruitList = new()
+        {
+            new Fruit { Id = "1", Name = "Banana", Price = 200 },
+            new Fruit { Id = "2", Name = "Apple", Price = 47 },
+            new Fruit { Id = "3", Name = "Orange", Price = 75 },
+            new Fruit { Id = "4", Name = "Lemon", Price = 123 },
+        };
+
+        [BindProperty]
+        public string SelectedFruit { get; set; } = "1";
+
+        public int? SelectedFruitPrice =>
+            _fruitList.FirstOrDefault(x => x.Id == SelectedFruit)?.Price;
+
+        public SelectList Options { get; set; }
+
+        public void OnGet()
+        {
+            Options = new SelectList(_fruitList, "Id", "Name");
+        }
+
+        public void OnPost()
+        {
+            Options = new SelectList(_fruitList, "Id", "Name");
+        }
+    }
+}
